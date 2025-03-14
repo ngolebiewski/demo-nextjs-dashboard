ab5b2514c53c89acd6f5cbe2471774e4816fad03
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-03-14 15:43:24 -0400

    ch 1-5 updates

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,5 +1,5 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import clsx from 'clsx'; // COOL< CLSX MAKES IT EASY! 
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
